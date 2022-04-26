2689d25f2cb23dac92805f2187cae3bbc4f7e1c3
Cristian <crissge2016@gmail.com>
2022-04-26 08:32:11 -0600

    Conexio a bd firebase y almacen de vehiculos funcional

--- app/src/main/java/com/example/rentacar/almacenarVehiculosFragment.java
+++ app/src/main/java/com/example/rentacar/almacenarVehiculosFragment.java
@@ -2,6 +2,7 @@ package com.example.rentacar;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
@@ -9,7 +10,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 
 public class almacenarVehiculosFragment extends Fragment {
@@ -25,11 +32,52 @@ public class almacenarVehiculosFragment extends Fragment {
         View vista = inflater.inflate(R.layout.fragment_almacenar_vehiculos, container, false);
 
         Button btn1 = vista.findViewById(R.id.btnregresarAlmacenVehiculo);
+        Button btn2 = vista.findViewById(R.id.btnAlmacenarAlmacenVehiculo);
 
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Navigation.findNavController(view).navigate(R.id.gestionVehiculos);
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText txtNombre, txtModelo, txtPlaca, txtMarca;
+                Spinner spnTipo, spnEstado;
+
+                txtNombre = vista.findViewById(R.id.nombreVehiculo);
+                txtModelo = vista.findViewById(R.id.modeloVehiculo);
+                txtPlaca = vista.findViewById(R.id.numeroPlacaVehiculo);
+                txtMarca = vista.findViewById(R.id.marcaVehiculo);
+                spnTipo = vista.findViewById(R.id.tipoVehiculoAlmacenarSpinner);
+                spnEstado = vista.findViewById(R.id.estadoVehiculoAlmacenarSpinner);
+
+                FirebaseFirestore db;
+                db = FirebaseFirestore.getInstance();
+
+                vehiculosClass nuevoVehiculo = new vehiculosClass(txtNombre.getText().toString(),
+                        txtMarca.getText().toString(),txtModelo.getText().toString(),
+                        spnTipo.getSelectedItem().toString(),spnEstado.getSelectedItem().toString(),0
+                        );
+
+                db.collection("vehiculos")
+                        .document(txtPlaca.getText().toString())
+                        .set(nuevoVehiculo)
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Toast.makeText(vista.getContext(),
+                                        "REGISTRO INSERTADO CON EXITO", Toast.LENGTH_SHORT).show();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(vista.getContext(),
+                                "HUBO UN ERROR EN EL PROCESO", Toast.LENGTH_SHORT).show();
+                    }
+                });
             }
         });
 
--- app/src/main/java/com/example/rentacar/estadoVehiculosFragment.java
+++ app/src/main/java/com/example/rentacar/estadoVehiculosFragment.java
@@ -3,10 +3,15 @@ package com.example.rentacar;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class estadoVehiculosFragment extends Fragment {
 
@@ -19,6 +24,36 @@ public class estadoVehiculosFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View vista = inflater.inflate(R.layout.fragment_estado_vehiculos, container, false);
+
+        TextView placa = vista.findViewById(R.id.numeroPlacaVehiculoEstado);
+        Button btnRegresar = vista.findViewById(R.id.btnRegresarEstadoVehiculo);
+
+        placa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView tv1, tv2, tv3, tv4;
+                Spinner sp1;
+
+                tv1 = vista.findViewById(R.id.nombreVehiculoEstado);
+                tv1.setVisibility(View.VISIBLE);
+                tv2 = vista.findViewById(R.id.modeloVehiculoEstado);
+                tv2.setVisibility(View.VISIBLE);
+                tv3 = vista.findViewById(R.id.MarcaVehiculoEstado);
+                tv3.setVisibility(View.VISIBLE);
+                tv4 = vista.findViewById(R.id.tipoVehiculoEstadoSpinner);
+                tv4.setVisibility(View.VISIBLE);
+                sp1 = vista.findViewById(R.id.estadoVehiculoSpinnerEstado);
+                sp1.setVisibility(View.VISIBLE);
+
+                Toast.makeText(view.getContext(), placa.getText().toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+        btnRegresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate(R.id.gestionVehiculos);
+            }
+        });
 
         return vista;
     }
