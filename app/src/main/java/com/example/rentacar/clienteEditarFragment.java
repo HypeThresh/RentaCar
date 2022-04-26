c2a722e8b8a465c87d47df3167e03f8bde2e5430
junior villalta <jr.villalta99@gmail.com>
2022-04-25 19:45:51 -0600

    Vistas de Registrar, Editar y Eliminar Cliente

--- app/src/main/java/com/example/rentacar/clienteEditarFragment.java
+++ app/src/main/java/com/example/rentacar/clienteEditarFragment.java
@@ -3,62 +3,56 @@ package com.example.rentacar;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link clienteEditarFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class clienteEditarFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public clienteEditarFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment clienteEditarFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static clienteEditarFragment newInstance(String param1, String param2) {
-        clienteEditarFragment fragment = new clienteEditarFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cliente_editar, container, false);
+        View vista = inflater.inflate(R.layout.fragment_cliente_editar, container, false);
+
+        TextView dui = vista.findViewById(R.id.duiClienteEditar);
+        Button btnRegresar = vista.findViewById(R.id.btnRegresarEditarCliente);
+
+        dui.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView tv1, tv2, tv3;
+
+                tv1 = vista.findViewById(R.id.nombreClienteEditar);
+                tv1.setVisibility(View.VISIBLE);
+
+                tv2 = vista.findViewById(R.id.telefonoClienteEditar);
+                tv2.setVisibility(View.VISIBLE);
+
+                tv3 = vista.findViewById(R.id.direccionClienteEditar);
+                tv3.setVisibility(View.VISIBLE);
+
+                Toast.makeText(view.getContext(), dui.getText().toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnRegresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate(R.id.gestionClientesFragment);
+            }
+        });
+
+        return vista;
     }
 }
