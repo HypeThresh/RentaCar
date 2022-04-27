d2a649b3863b07da21e8fea93604cdd3298a8906
Cristian <crissge2016@gmail.com>
2022-04-27 11:31:53 -0600

    Actualizar funcionando

--- app/src/main/java/com/example/rentacar/editarVehiculosFragment.java
+++ app/src/main/java/com/example/rentacar/editarVehiculosFragment.java
@@ -2,6 +2,7 @@ package com.example.rentacar;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
@@ -13,9 +14,18 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.SetOptions;
+import com.google.firestore.v1.WriteResult;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class editarVehiculosFragment extends Fragment {
 
@@ -32,7 +42,21 @@ public class editarVehiculosFragment extends Fragment {
 
         TextView placa = vista.findViewById(R.id.numeroPlacaVehiculoEditar);
         Button btnRegresar = vista.findViewById(R.id.btnRegresarEditarVehiculo);
-        Button btnEditar = vista.findViewById(R.id.btnEditarVehiculo);
+        Button btnEditar = vista.findViewById(R.id.btnEditarVehiculoEdit);
+
+        TextView tv1, tv2, tv3;
+        Spinner sp1, sp2;
+
+        tv1 = vista.findViewById(R.id.nombreVehiculoEditar);
+        tv1.setVisibility(View.VISIBLE);
+        tv2 = vista.findViewById(R.id.modeloVehiculoEditar);
+        tv2.setVisibility(View.VISIBLE);
+        tv3 = vista.findViewById(R.id.MarcaVehiculoEditar);
+        tv3.setVisibility(View.VISIBLE);
+        sp1 = vista.findViewById(R.id.tipoVehiculoEditarSpinner);
+        sp1.setVisibility(View.VISIBLE);
+        sp2 = vista.findViewById(R.id.estadoVehiculoEditarSpinner);
+        sp2.setVisibility(View.VISIBLE);
 
         placa.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,19 +68,6 @@ public class editarVehiculosFragment extends Fragment {
                         .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
                             @Override
                             public void onSuccess(DocumentSnapshot documentSnapshot) {
-                            TextView tv1, tv2, tv3;
-                            Spinner sp1, sp2;
-
-                            tv1 = vista.findViewById(R.id.nombreVehiculoEditar);
-                            tv1.setVisibility(View.VISIBLE);
-                            tv2 = vista.findViewById(R.id.modeloVehiculoEditar);
-                            tv2.setVisibility(View.VISIBLE);
-                            tv3 = vista.findViewById(R.id.MarcaVehiculoEditar);
-                            tv3.setVisibility(View.VISIBLE);
-                            sp1 = vista.findViewById(R.id.tipoVehiculoEditarSpinner);
-                            sp1.setVisibility(View.VISIBLE);
-                            sp2 = vista.findViewById(R.id.estadoVehiculoEditarSpinner);
-                            sp2.setVisibility(View.VISIBLE);
 
                             String nombre = "" + documentSnapshot.getData().get("nombre");
                             tv1.setText(nombre);
@@ -64,23 +75,6 @@ public class editarVehiculosFragment extends Fragment {
                             tv2.setText(modelo);
                             String marca = "" + documentSnapshot.getData().get("marca");
                             tv3.setText(marca);
-                            String tip = "" + documentSnapshot.getData().get("tipo");
-                            if (tip == "Coche"){
-                                sp1.setSelection(1);
-                            }else if (tip == "Microbus"){
-                                sp1.setSelection(2);
-                            }else if (tip == "Furgonetas de carga"){
-                                sp1.setSelection(3);
-                            }else if (tip == "Camion") {
-                                sp1.setSelection(4);
-                            }
-
-                            String dis = "" + documentSnapshot.getData().get("estado");
-                            if (dis == "Disponible"){
-                                sp1.setSelection(1);
-                            }else if (dis == "Alquilado"){
-                                sp1.setSelection(2);
-                            }
                             Toast.makeText(vista.getContext(), "Datos encontrados", Toast.LENGTH_SHORT).show();
                         }
                 });
@@ -95,7 +89,40 @@ public class editarVehiculosFragment extends Fragment {
         btnEditar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                
+                vehiculosClass actualizarVehiculo = new vehiculosClass(tv1.getText().toString(),
+                        tv3.getText().toString(),tv2.getText().toString(),
+                        sp1.getSelectedItem().toString(),sp2.getSelectedItem().toString(),0);
+
+                FirebaseFirestore db;
+                db = FirebaseFirestore.getInstance();
+                DocumentReference docRef = db.collection("vehiculos").document(placa.getText().toString());
+                docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if (task.isSuccessful()) {
+                            DocumentSnapshot document = task.getResult();
+                            if (document.exists()) {
+                                Map<String, Object> update = new HashMap<>();
+                                update.put("nombre", tv1.getText().toString());
+                                update.put("modelo", tv2.getText().toString());
+                                update.put("marca", tv3.getText().toString());
+                                update.put("tipo", sp1.getSelectedItem().toString());
+                                update.put("estado", sp2.getSelectedItem().toString());
+
+                                db.collection("vehiculos")
+                                    .document(placa.getText().toString())
+                                    .set(update, SetOptions.merge());
+                                Toast.makeText(vista.getContext(), "Actualizado con exito", Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(vista.getContext(), "Id no encontrado", Toast.LENGTH_SHORT).show();
+                            }
+                        } else {
+                            Toast.makeText(vista.getContext(), "Error al editar", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+
             }
         });
 
