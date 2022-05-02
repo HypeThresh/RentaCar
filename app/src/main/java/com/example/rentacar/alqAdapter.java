9643e5f9110917ea0d97142b9bc3752a78c625e9
Cristian <crissge2016@gmail.com>
2022-05-02 15:41:59 -0600

    eliminacion de alquileres en lista completa

--- app/src/main/java/com/example/rentacar/alqAdapter.java
+++ app/src/main/java/com/example/rentacar/alqAdapter.java
@@ -1,10 +1,19 @@
 package com.example.rentacar;
 
+import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
 
@@ -18,6 +27,7 @@ public class alqAdapter extends RecyclerView.Adapter<alqAdapter.ViewHolder> {
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
         private final TextView dui,placa,estado,fechaIni,fechaFin;
+        ImageView editar, eliminar;
 
         public ViewHolder(View view) {
             super(view);
@@ -27,6 +37,9 @@ public class alqAdapter extends RecyclerView.Adapter<alqAdapter.ViewHolder> {
             estado = view.findViewById(R.id.viewEstado);
             fechaIni = view.findViewById(R.id.viewFecIni);
             fechaFin = view.findViewById(R.id.viewFecFin);
+
+            editar = view.findViewById(R.id.imageViewEditar);
+            eliminar = view.findViewById(R.id.imageViewEliminar);
         }
         public TextView getTextViewDui() { return dui; }
         public TextView getTextViewPlaca() { return placa; }
@@ -52,6 +65,41 @@ public class alqAdapter extends RecyclerView.Adapter<alqAdapter.ViewHolder> {
         viewHolder.estado.setText("Estado: "+localDataSet.get(position).getAlquilado());
         viewHolder.fechaIni.setText("Fecha de inicio:\n"+localDataSet.get(position).getFechaIni());
         viewHolder.fechaFin.setText("Fecha de entrega:\n"+localDataSet.get(position).getFechaFin());
+
+        viewHolder.eliminar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(view.getContext());
+                builder.setCancelable(true);
+                builder.setTitle("Eliminar");
+                String mensaje = "Deseas eliminar el registro? ";
+                builder.setMessage(mensaje);
+                builder.setPositiveButton("Confirmar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        FirebaseFirestore db;
+                        db = FirebaseFirestore.getInstance();
+                        db.collection("alquileres").document(localDataSet.get(position).getDocumento())
+                                .delete()
+                                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void unused) {
+                                        Toast.makeText(view.getContext(),"Eliminado con exito",Toast.LENGTH_LONG).show();
+                                        Navigation.findNavController(view).navigate(R.id.inicioFragment);
+                                    }
+                                });
+                    }
+                });
+                builder.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(view.getContext(),"Operacion cancelada con exito",Toast.LENGTH_LONG).show();
+                    }
+                });
+                builder.create();
+                builder.show();
+            }
+        });
     }
 
     // Return the size of your dataset (invoked by the layout manager)
